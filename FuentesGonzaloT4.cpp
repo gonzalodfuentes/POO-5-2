9d7e40b676e734cd469b8f55b9529ef1e12c92af
gonzalodfuentes <130400825+gonzalodfuentes@users.noreply.github.com>
2024-04-15 17:45:36 -0300

    Update FuentesGonzaloT4.cpp

--- FuentesGonzaloT4.cpp
+++ FuentesGonzaloT4.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-	int menuopcion, a, b;
+	int menuopcion;
+	
+	printf(" 1. Es bisiesto? \n 2. Entero y/o positivo? \n 3. Pago de entradas \n 4. Catering \n 5. Vinicultores\n");
 	
 	printf("Que opcion elige?: ");
 	scanf("%d", &menuopcion);
@@ -14,25 +16,34 @@ int main()
 		{
 			int a;
 			
+			// Ingresar el año
 			printf("Cual desea averiguar si es bisiesto o no?: ");
 			scanf("%d", &a);
 			
+			
+			// Condicionales para saber si el año es bisiesto
 			if((a % 4) == 0 && (a % 100) != 0)
 			{
+				// Divisible por 4 pero no por 100
 				printf("El %d es bisiesto.", a);
 			}
 			else if((a % 4) != 0)
 			{
+				// No divisible por 4
 				printf("El %d no es bisiesto.", a);
 			}
 			else if((a % 400) == 0 && (a % 100) == 0)
 			{
+				// Divisible por 400 y divisible por 100
 				printf("El %d es bisiesto.", a);
 			}
 			else
 			{
+				// Ninguna de las anteriores
 				printf("El %d no es bisiesto.", a);
 			}
+			
+			break;
 		}
 		
 		
@@ -40,10 +51,12 @@ int main()
 		case 2:
 		{
 			float n;
+			int i;
 			
 			printf("Ingrese un numero: ");
 			scanf("%f", &n);
 			
+			// Condicionales
 			if(n > 0)
 			{
 				printf("Es positivo.");
@@ -51,11 +64,51 @@ int main()
 			else if(n == 0)
 			{
 				printf("Es 0, entero.");
+				
+				// Retorna todo
+				return 0;
 			}
 			else
 			{
+				// Ninguna de las anteriores
 				printf("Es negativo.");
 			}
+			
+			// Ciclo que se repite infinitamente para saber si es decimal o entero
+			for(i = 0; 0 < 1; i++)
+			{
+				
+				if(n > 0)
+				{
+					// Lo resta constantemente
+					n -= 1;
+				}
+				else if(n < 0)
+				{
+					// Lo suma constantemente
+					n += 1;
+				}
+				
+				if(n >= 0 && n < 1)
+				{
+					// Una vez el numero sea 0 o este entre 0 y 1 se cierra el ciclo solo
+					break;
+				}
+			}
+			
+			
+			// Condicional de si es igual a 0 es entero, sino, decimal
+			if(n == 0)
+			{
+				printf("\nEs entero.");
+			}
+			else
+			{
+				printf("\nEs decimal.");
+			}
+			
+			break;
+			
 		}
 		
 		
@@ -64,12 +117,15 @@ int main()
 		{
 			int pre, ent;
 			
+			// Precio y personas
 			printf("Cuanto salen las entradas?: ");
 			scanf("%d", &pre);
 			
 			printf("Cuantas entradas quiere la persona?: ");
 			scanf("%d", &ent);
 			
+			
+			// Condicionales para saber su descuento
 			if(ent == 1)
 			{
 				printf("Debera pagar %d por la entrada. Sin descuento", pre);
@@ -77,27 +133,30 @@ int main()
 			}
 			else if(ent == 2)
 			{
-				pre += 10 * pre / 100;
+				pre = 90 * pre / 100;
 				printf("Debera pagar %d por las entradas. Con 10%% de descuento.", pre);
 				break;
 			}
 			else if(ent == 3)
 			{
-				pre += 15 * pre / 100;
+				pre = 85 * pre / 100;
 				printf("Debera pagar %d por las entradas. Con 15%% de descuento.", pre);
 				break;
 			}
 			else if(ent == 4)
 			{
-				pre += 16 * pre / 100;
+				pre = 80 * pre / 100;
 				printf("Debera pagar %d por las entradas. Con 20%% de descuento.", pre);
 				break;
 			}
 			else
 			{
+				// Si ingresa 0 o +5
 				printf("Invalido.");
 				break;
 			}
+			
+			break;
 		}
 		
 		
@@ -106,9 +165,11 @@ int main()
 		{
 			int p, cant;
 			
+			// Cantidad de personas
 			printf("Cuantas personas son?: ");
 			scanf("%d", &p);
 			
+			// Condicionales para saber cuanto van a pagar en total
 			if(p > 200 && p <= 300)
 			{
 				cant = p * 5500;
@@ -122,48 +183,65 @@ int main()
 				cant = p * 6500;
 			}
 			
+			// Precio
 			printf("Va tener que pagar por plato/persona, unos $%d pesos.", cant);
+			
+			break;
 		}
 		
 		
 		// 5
 		case 5:
 		{
-			int ti, ta, pr, ki, uva;
+			int ti, ta, pr, ki, uva, i, precio;
 			
-			printf("De que tipo es la uva que usted entrega? (1- A || 2- B): ");
-			scanf("%d", &ti);
-			
-			printf("Y de que tamano? (1- 1 || 2- 2): ");
-			scanf("%d", &ta);
-			
-			if(ti == 1 && ta == 1)
-			{
-				pr = 200;
-			}
-			else if(ti == 1 && ta == 2)
-			{
-				pr = 300;
-			}
-			else if(ti == 2 && ta == 1)
-			{
-				pr = 300;
-			}
-			else if(ti == 2 && ta == 2)
-			{
-				pr = 500;
-			}
-			else
-			{
-				printf("Invalido");
-			}
-			
-			printf("Finalmente cuantos kilos de uva vendera?: ");
+			// Los kilos que va a vender
+			printf("Cuantos kilos vas a vender?: ");
 			scanf("%d", &ki);
 			
-			uva = ki * pr;
+			// Ciclo que se repite por cantidad de kilos que venda
+			for(i = 0; i < ki; i++)
+			{
+				// El tipo de uva y su tamaño
+				printf("De que tipo es la uva que usted entrega del kilo %d? (1- A || 2- B): ", i+1);
+				scanf("%d", &ti);
 			
-			printf("Vas a recibir $%d.", uva);
-		}
+				printf("Y de que tamano? (1- 1 || 2- 2): ");
+				scanf("%d", &ta);
+			
+				// Condicionales que determinan su precio
+				if(ti == 1 && ta == 1)
+				{
+					pr = 200;
+				}
+				else if(ti == 1 && ta == 2)
+				{
+					pr = 300;
+				}
+				else if(ti == 2 && ta == 1)
+				{
+					pr = 300;
+				}
+				else if(ti == 2 && ta == 2)
+				{
+					pr = 500;
+				}
+				else
+				{
+					// Por si ingresa 0 o +3 en algunas de las 2 preguntas anteriores
+					printf("Invalido");
+				}
+			
+				// Acumula el precio de cada kilo una vez termine de pasarlos a todos
+				precio += pr;
+				
+				pr = 0;
+			}
+			
+			// Lo que va a recibir de la venta
+			printf("Vas a recibir $%d.", precio);
+			
+			break;
+		}	
 	}
 }
