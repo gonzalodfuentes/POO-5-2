970a34c5dea504de5ab958a1d52c4eea4bcb71f6
gonzalodfuentes <130400825+gonzalodfuentes@users.noreply.github.com>
2024-08-26 17:00:41 -0300

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,18 @@
+// FUENTES_Punto1
+
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i = 0; i < 101; i++)
+		{
+		    if(i % 2 == 0)
+		    {
+		        System.out.println("Es Par => " + i);
+		    }
+		    else
+		    {
+		        System.out.println("Es Impar => " + i);
+		    }
+		}
+	}
+}
