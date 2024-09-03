b5173622cf591be579c6223fbe0e0de46b28c24c
gonzalodfuentes <130400825+gonzalodfuentes@users.noreply.github.com>
2024-09-03 14:50:14 -0300

    Create Ejercicio7.java

---
+++ Ejercicio7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  // Importar la clase Scanner
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        Scanner teclado = new Scanner(System.in);
+        
+        String nombre;
+        
+        nombre = "Gonzalo";
+        
+        System.out.println("Bienvenido " + nombre);
+    }
+}
